fef078281e4e2d2ef06399a8610336d060dfb850
Pavel N. Krivitsky <pavel@uow.edu.au>
2016-03-30 10:45:14 +0000

    Updated license and citation data, and added copyright headers.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.rank/trunk@14388 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtchangestats_rank.h
+++ src/wtchangestats_rank.h
@@ -1,3 +1,12 @@
+/*  File src/wtchangestats_rank.h in package ergm.rank, part of the Statnet suite
+ *  of packages for network analysis, http://statnet.org .
+ *
+ *  This software is distributed under the GPL-3 license.  It is free,
+ *  open source, and has the attribution requirements (GPL Section 7) at
+ *  http://statnet.org/attribution
+ *
+ *  Copyright 2008-2016 Statnet Commons
+ */
 #ifndef WTCHANGESTATS_RANK_H
 #define WTCHANGESTATS_RANK_H
 
