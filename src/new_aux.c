b0be49ab0bbf1e5ff2c42ef2d214cc247cf94151
kinguinpenguin <patdude102@gmail.com>
2025-06-11 20:37:22 +1000

    ergm.rank implementation (??)

---
+++ src/new_aux.c
@@ -0,0 +1,125 @@
+/*  File src/wtchangestats_test.c in package ergm, part of the
+ *  Statnet suite of packages for network analysis, https://statnet.org .
+ *
+ *  This software is distributed under the GPL-3 license.  It is free,
+ *  open source, and has the attribution requirements (GPL Section 7) at
+ *  https://statnet.org/attribution .
+ *
+ *  Copyright 2003-2025 Statnet Commons
+ */
+#include "ergm_wtedgetree.h"
+#include "ergm_wtchangestat.h"
+#include "ergm_storage.h"
+
+typedef unsigned int Vertex;
+typedef Vertex Pair[2];
+
+/*WtC_CHANGESTAT_FN(c_test_abs_sum_minus_5){
+  GET_STORAGE(double, stored_sum_ptr);
+  double sum = *stored_sum_ptr;
+    CHANGE_STAT[0] = -fabs(sum-5);
+    CHANGE_STAT[0] += fabs(sum-5 + weight - edgestate);
+}
+
+WtI_CHANGESTAT_FN(i_test_abs_sum_minus_5){
+  ALLOC_STORAGE(1, double, sum);
+  *sum = 0;
+  EXEC_THROUGH_NET_EDGES(tail, e1, head, y, {
+      *sum+=y;
+      head=head; e1=e1; // Prevent a compiler warning.
+    });
+}
+
+WtU_CHANGESTAT_FN(u_test_abs_sum_minus_5){
+  GET_STORAGE(double, sum);
+  *sum += weight-edgestate;
+}
+
+WtS_CHANGESTAT_FN(s_test_abs_sum_minus_5){
+  GET_STORAGE(double, sum);
+  // Storage uninitialized: compute from scratch.
+  if(!sum){
+    double sum = 0;
+    EXEC_THROUGH_NET_EDGES(tail, e1, head, y, {
+	sum+=y;
+	head=head; e1=e1; // Prevent a compiler warning.
+      });
+    CHANGE_STAT[0] = fabs(sum-5);
+  }else{ // Storage initialized: use it.
+    CHANGE_STAT[0] = fabs(*sum-5);
+  }
+}
+
+WtC_CHANGESTAT_FN(c_test_abs_sum_minus_5_no_s){c_test_abs_sum_minus_5(tail, head, weight, mtp, nwp, edgestate);}
+WtI_CHANGESTAT_FN(i_test_abs_sum_minus_5_no_s){i_test_abs_sum_minus_5(mtp, nwp);}
+WtU_CHANGESTAT_FN(u_test_abs_sum_minus_5_no_s){u_test_abs_sum_minus_5(tail, head, weight, mtp, nwp, edgestate);}*/
+
+
+
+WtI_CHANGESTAT_FN(i__dsociomatrix){
+  double R[N_NODES][N_NODES];
+  
+  // Now, populate the sociomatrix.
+  EXEC_THROUGH_NET_EDGES(t, h, e, w, {
+      R[t][h] = w;
+    });
+
+  ALLOC_AUX_SOCIOMATRIX(Pair, sm);
+  for (Vertex t = 1; t <= N_NODES; t++) {
+    for (Vertex j = 1; j <= N_NODES; j++) {
+      if (t == j || R[t][j] == 0) continue;
+
+      double r_j = R[t][j];
+      Vertex below = 0;
+      Vertex above = 0;
+
+      for (Vertex k = 1; k <= N_NODES; k++) {
+        if (k == t || k == j || R[t][k] == 0) continue;
+
+        if (R[t][k] == r_j + 1) below = k;
+        if (R[t][k] == r_j - 1) above = k;
+      }
+
+      sm[t][j][0] = below;
+      sm[t][j][1] = above;
+    }
+  }
+}
+
+WtU_CHANGESTAT_FN(u__dsociomatrix){
+  GET_AUX_STORAGE(Pair, sm);
+}
+
+WtF_CHANGESTAT_FN(f__dsociomatrix){
+  FREE_AUX_SOCIOMATRIX;
+}
+
+/*
+WtC_CHANGESTAT_FN(c_dsociomatrix){
+  GET_AUX_STORAGE(double *, sm);
+  
+      Dyad pos = tail-1 + (head-1)*N_NODES;
+      CHANGE_STAT[pos] = weight - sm[tail][head];
+}
+
+
+WtI_CHANGESTAT_FN(i__sum){
+  ALLOC_AUX_STORAGE(1, double, sum);
+  *sum = 0;
+  EXEC_THROUGH_NET_EDGES(tail, e1, head, y, {
+      *sum+=y;
+      (void) head; (void) e1; // Prevent a compiler warning.
+    });
+}
+
+WtU_CHANGESTAT_FN(u__sum){
+  GET_AUX_STORAGE(double, sum);
+  *sum += weight-edgestate;
+}
+
+WtC_CHANGESTAT_FN(c_test_abs_sum_minus_5_aux){
+  GET_AUX_STORAGE(double, stored_sum_ptr);
+  double sum = *stored_sum_ptr;
+    CHANGE_STAT[0] = -fabs(sum-5);
+    CHANGE_STAT[0] += fabs(sum-5 + weight - edgestate);
+}*/
